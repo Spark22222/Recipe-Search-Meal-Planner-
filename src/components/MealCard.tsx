617d9cc15fde651bea8b8fbe404c98a24f21c457
zhu <zzy1320049874@gmail.com>
2026-05-22 16:37:33 +1000

    Add recipe detail modal

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,18 +1,20 @@
 import type { Meal } from "../types/meal";
 
-interface MealCardProps{
-    meal:Meal
+interface MealCardProps {
+    meal: Meal
+    onSelect:() => void
 }
 
-const MealCard = ({meal}:MealCardProps) => {
+const MealCard = ({ meal,onSelect }: MealCardProps) => {
 
-    return(
-            <div>
-              <img src={meal.strMealThumb} alt={meal.strMeal} width='200'></img>
-              <h3>{meal.strMeal}</h3>
-              <p>{meal.strCategory}</p>
-              <p>{meal.strArea}</p>
-            </div>
+    return (
+        <div>
+            <img src={meal.strMealThumb} alt={meal.strMeal} width='200'></img>
+            <h3>{meal.strMeal}</h3>
+            <p>{meal.strCategory}</p>
+            <p>{meal.strArea}</p>
+            <button onClick={onSelect}>SHOW MORE</button>
+        </div>
     )
 }
 
